6168bf85d83542f8397f3a0769070c00b749364c
Felipe <felipecerboncinix@gmail.com>
2024-03-22 13:58:54 -0300

    Initial commit

---
+++ src/main/java/com/cerbon/daylight_zombies/DaylightZombies.java
@@ -0,0 +1,10 @@
+package com.cerbon.daylight_zombies;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DaylightZombies.MOD_ID)
+public class DaylightZombies {
+    public static final String MOD_ID = "daylight_zombies";
+
+    public DaylightZombies() {}
+}
